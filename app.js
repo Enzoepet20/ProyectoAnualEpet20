7c967b67038844eec2a6597d15831974c9267607
enzoepet20 <enzobeltran.epet20.5to1ra.b@gmail.com>
2021-08-16 17:42:17 -0300

    modificacion de diseño carrusel+ js

--- app.js
+++ app.js
@@ -13,3 +13,13 @@ const showImg = () => {
 const interval = () => {
     setInterval(showImg, 2500);
 }
+const grande = document.querySelector('.grande');
+const punto = document.querySelectorAll('.punto');
+
+punto.forEach(cadaPunto, i) => {
+    punto[i].addEventListener('click', () => {
+        let posicion = i;
+        let operacion = posicion * -25;
+        grande.style.transform = `translateX{${operacion}}`
+    })
+}
