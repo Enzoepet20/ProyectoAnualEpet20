304d9fd69a797c09540557edd6b12472c4928a7a
enzoepet20 <enzobeltran.epet20.5to1ra.b@gmail.com>
2021-08-16 18:29:15 -0300

    carrousel basico creado

--- app.js
+++ app.js
@@ -15,11 +15,16 @@ const interval = () => {
 }
 const grande = document.querySelector('.grande');
 const punto = document.querySelectorAll('.punto');
-
-punto.forEach(cadaPunto, i) => {
+punto.forEach((cadaPunto, i) => {
     punto[i].addEventListener('click', () => {
         let posicion = i;
         let operacion = posicion * -25;
-        grande.style.transform = `translateX{${operacion}}`
+        grande.style.transform = `translateX(${ operacion }%)`;
+
+        punto.forEach((cadaPunto, i) => {
+            punto[i].classList.remove('activo');
+        })
+        punto[i].classList.add('activo');
+
     })
-}
+})
