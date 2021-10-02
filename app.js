b5219f9bfa4372ab18867b7bb1a71e1022359220
Rayen Millahual <rcmillahual@gmail.com>
2021-10-02 15:02:39 -0300

    enlace de js del footer en html

--- app.js
+++ app.js
@@ -23,3 +23,54 @@ punto.forEach((cadaPunto, i) => {
 let boton_wpp = document.getElementById("boton_wpp");
 boton_wpp.innerHTML += `<a href="https://api.whatsapp.com/send?phone=+5492994181952" class="btn-wsp" target="_blank"><img src="icono/WhatsApp-logo.png" class="btn-wsp" alt="">
 </a> `
+let ctfooter = document.getElementById("ct-footer");
+ctfooter.innerHTML += ` <footer class="ct-footer">
+<div class="container">
+    <ul class="ct-footer-list text-center-sm">
+        <li>
+            <h2 class="ct-footer-list-header">
+                Sobre nosotros
+            </h2>
+            <ul>
+                <li>
+                    <a href="">Empresa familiar</a>
+                </li>
+                <li>
+                    <a href="">Clientes</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <h2 class="ct-footer-list-header">
+                Servicios
+            </h2>
+            <ul>
+                <li>
+                    <a href="">Sales</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <h2 class="ct-footer-list-header">
+                Contacto
+            </h2>
+            <ul>
+                <li>
+                    <a href="https://api.whatsapp.com/send?phone=+5492994181952">wpp</a>
+                </li>
+                <li>
+                    <a href="https://www.facebook.com/rayen.millahual.12">fc</a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/christian_beltran79/">ig</a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/Rayen_raa">tw</a>
+                </li>
+                <li>
+                    <a href="">lk</a>
+                </li>
+            </ul>
+        </li>
+
+</footer>`
