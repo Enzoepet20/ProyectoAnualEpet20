2b8800b5730055016a135533a6526c2e7bc15aa3
Rayen <rcmillahual@gmail.com>
2021-10-24 12:04:23 -0300

    me re podri

--- app.js
+++ app.js
@@ -32,49 +32,51 @@ let boton_wpp = document.getElementById("boton_wpp");
 boton_wpp.innerHTML += `<a href="https://api.whatsapp.com/send?phone=+5492994181952" class="btn-wsp" target="_blank"><img src="icono/WhatsApp-logo.png" class="btn-wsp" alt="">
 </a> `
 let ctfooter = document.getElementById("ct-footer");
-ctfooter.innerHTML += `<footer class="page-footer font-small special-color-dark pt-4">
-
-<!-- Footer Elements -->
+ctfooter.innerHTML += ` <footer class="ct-footer">
 <div class="container">
-
-  <!-- Social buttons -->
-  <ul class="list-unstyled list-inline text-center">
-    <li class="list-inline-item">
-      <a class="btn-floating btn-fb mx-1">
-        <i class="fab fa-facebook-f"> </i>
-      </a>
-    </li>
-    <li class="list-inline-item">
-      <a class="btn-floating btn-tw mx-1">
-        <i class="fab fa-twitter"> </i>
-      </a>
-    </li>
-    <li class="list-inline-item">
-      <a class="btn-floating btn-gplus mx-1">
-        <i class="fab fa-google-plus-g"> </i>
-      </a>
-    </li>
-    <li class="list-inline-item">
-      <a class="btn-floating btn-li mx-1">
-        <i class="fab fa-linkedin-in"> </i>
-      </a>
-    </li>
-    <li class="list-inline-item">
-      <a class="btn-floating btn-dribbble mx-1">
-        <i class="fab fa-dribbble"> </i>
-      </a>
-    </li>
-  </ul>
-  <!-- Social buttons -->
-
-</div>
-<!-- Footer Elements -->
-
-<!-- Copyright -->
-<div class="footer-copyright text-center py-3">© 2020 Copyright:
-  <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
-</div>
-<!-- Copyright -->
+    <ul class="ct-footer-list text-center-sm">
+        <li>
+            <h2 class="ct-footer-list-header">
+                Sobre nosotros
+            </h2>
+            <ul>
+                <li>
+                    <a href="">Empresa familiar</a>
+                </li>
+                <li>
+                    <a href="">Clientes</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <h2 class="ct-footer-list-header">
+                Servicios
+            </h2>
+            <ul>
+                <li>
+                    <a href="">Sales</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <h2 class="ct-footer-list-header">
+                Contacto
+            </h2>
+            <ul>
+                <li>
+                    <a href="https://api.whatsapp.com/send?phone=+5492994181952">Whatsapp</a>
+                </li>
+                <li>
+                    <a href="https://www.facebook.com/rayen.millahual.12">Facebook</a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/christian_beltran79/">Instagram</a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/Rayen_raa">Twitter</a>
+                </li>
+            </ul>
+        </li>
 
 </footer>`
 
